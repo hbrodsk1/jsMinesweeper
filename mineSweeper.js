1328d351bb0cb3a6276250d2c32e529df2dbb2f5
Harry Brodsky <hbrodsk1@binghamton.edu>
2016-11-12 14:23:29 -0500

    Plant flags on board

--- mineSweeper.js
+++ mineSweeper.js
@@ -48,8 +48,79 @@ var gameboard = (function() {
 
 var gamePlay = (function() {
 
+	var flagCount = 9;
+
+	var go = function() {
+		updateFlagCount();
+		$('.unit').click(function() {
+			if($(this).hasClass('mine')) {
+				$(this).html("Mine");
+         		gameOver();
+     		} else {
+     			$(this).html(mineInfo($(this)));
+     		}
+    	})
+
+    	$('.unit').contextmenu(function() {
+    		if(flagCount > 0 && !$(this).hasClass('flagged')) {
+    			$(this).addClass('flagged').html('flag');
+    			flagCount--;
+    			updateFlagCount();
+    		}
+    	});
+	}
+
+	var updateFlagCount = function() {
+		$('#flags').html("Flags Remaining: " + flagCount);
+	}		
 	
+	var mineInfo = function(element) {
+
+		var array = [];
+
+		var elementId = Number(element.attr("id"));
+		var top = elementId - 9;
+		var bottom = elementId + 9;
+		var before = elementId - 1;
+		var after = elementId + 1;
+
+		array.push($top = $('#' + (top).toString()));
+		array.push($bottom = $('#' + (bottom).toString()));
+		array.push($before = $('#' + (before).toString()));
+		array.push($after = $('#' + (after).toString())); 
+
+		return mineCount(array);
+	}
+
+	var mineCount = function(array) {
+		var counter = 0;
+
+		$.each( array, function( index, value) {
+			if(value.hasClass("mine")) {
+				counter++;
+			}
+		});
+		return counter;
+	}
+
+	var gameOver = function() {
+		$('.mine').html("mine").css("background-color", "red");
+		$('.restart').show().click(function() {
+			$('.unit').remove();
+			$('.mine').remove();
+			$('.restart').hide();
+
+			gameboard.initialize(9, 9);
+			go();
+		});
+		
+	}
+
+	return {
+		go: go,
+	};
 
 }());
 
 gameboard.initialize(9, 9);
+gamePlay.go();
